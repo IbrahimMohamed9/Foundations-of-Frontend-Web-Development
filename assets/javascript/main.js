f84bceb713379865b5675d0ea95441a7f555182c
IbrahimMoatazMohamed <ibrahim.mohamed@stu.ibu.edu.ba>
2024-03-07 09:57:41 +0100

    finish logo animation

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,4 @@
+//dots in main-title
 const mainTitles = document.querySelectorAll(".main-title");
 
 mainTitles.forEach((mainTitle) => {
@@ -16,89 +17,7 @@ mainTitles.forEach((mainTitle) => {
   });
 });
 
-const shareIcons = document.querySelectorAll(".fa-solid.fa-share");
-
-shareIcons.forEach((shareIcon, index) => {
-  shareIcon.addEventListener("mouseenter", () => {
-    const parentDiv = shareIcon.parentElement;
-    const shareList = parentDiv.querySelector(".font-share-icons");
-    shareList.style.display = "grid";
-
-    shareIcons.forEach((shareIcon1, index1) => {
-      if (index1 !== index) {
-        removeOpenShare(index1);
-      }
-    });
-  });
-});
-shareIcons.forEach((shareIcon, index) => {
-  shareIcon.addEventListener("click", () => {
-    const parentDiv = shareIcon.parentElement;
-    const shareList = parentDiv.querySelector(".font-share-icons");
-    if (shareList.style.display === "grid") {
-      shareList.style.display = "none";
-    } else {
-      shareList.style.display = "grid";
-    }
-  });
-});
-
-function removeOpenShare(index) {
-  const shareIcon = shareIcons[index];
-  const parentDiv = shareIcon.parentElement;
-  const shareList = parentDiv.querySelector(".font-share-icons");
-  shareList.style.display = "none";
-}
-
-const scrollers = document.querySelectorAll(".scroller");
-if (!window.matchMedia("(prefers-reduced-motion: reduce").matches) {
-  addAnimation();
-}
-function addAnimation() {
-  scrollers.forEach((scroller) => {
-    scroller.setAttribute("data-animated", true);
-
-    const scrollerInner = scroller.querySelector(".scroller__inner");
-    const scrollerConternt = Array.from(scrollerInner.children);
-
-    scrollerConternt.forEach((item) => {
-      const duplicatedItem = item.cloneNode(true);
-      duplicatedItem.setAttribute("aria-hidden", true);
-      scrollerInner.appendChild(duplicatedItem);
-    });
-  });
-}
-
-let buttons = document.querySelectorAll(".info .row .content button");
-
-buttons.forEach((button, index) => {
-  button.addEventListener("click", function () {
-    let span = this.querySelector("span");
-    let paragraph = this.nextElementSibling;
-
-    paragraph.classList.toggle("show");
-
-    if (paragraph.classList.contains("show")) {
-      span.textContent = "";
-    } else {
-      span.textContent = "More";
-    }
-    removeOpen(index);
-  });
-});
-
-function removeOpen(clickedIndex) {
-  buttons.forEach((button, index) => {
-    if (index !== clickedIndex) {
-      let paragraph = button.nextElementSibling;
-      let span = button.querySelector("span");
-
-      paragraph.classList.remove("show");
-      span.textContent = "More";
-    }
-  });
-}
-
+//change heart in hover header
 const heartIcon = document.querySelector(".heart.fa-regular");
 
 heartIcon.addEventListener("mouseenter", () => {
@@ -111,6 +30,7 @@ heartIcon.addEventListener("mouseleave", () => {
   heartIcon.classList.add("fa-regular");
 });
 
+//dark theme
 const icon = document.getElementById("switch");
 if (icon) {
   icon.addEventListener("click", function () {
@@ -124,25 +44,3 @@ if (icon) {
 } else {
   console.error("The element with ID 'switch' was not found.");
 }
-
-//add to cart
-var modal = document.getElementById("myModal");
-
-var btns = document.querySelectorAll(".pckbtn");
-
-function handleClick() {
-  modal.classList.add("active");
-  document.body.classList.add("fix");
-}
-
-btns.forEach((button) => button.addEventListener("click", handleClick));
-
-document.querySelector(".x").addEventListener("click", function () {
-  modal.classList.remove("active");
-  document.body.classList.remove("fix");
-});
-
-document.querySelector(".checkout-btn").addEventListener("click", function () {
-  modal.classList.remove("active");
-  document.body.classList.remove("fix");
-});
