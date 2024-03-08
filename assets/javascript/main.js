6b3bace2f0f81df077949813b3e1098f5484f58b
IbrahimMoatazMohamed <ibrahim.mohamed@stu.ibu.edu.ba>
2024-03-08 11:31:38 +0100

    finish pay btn

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -44,3 +44,25 @@ if (icon) {
 } else {
   console.error("The element with ID 'switch' was not found.");
 }
+
+//add to cart
+var modal = document.getElementById("myModal");
+
+var btns = document.querySelectorAll(".pckbtn");
+
+function handleClick() {
+  modal.classList.add("active");
+  document.body.classList.add("fix");
+}
+
+btns.forEach((button) => button.addEventListener("click", handleClick));
+
+document.querySelector(".x").addEventListener("click", function () {
+  modal.classList.remove("active");
+  document.body.classList.remove("fix");
+});
+
+document.querySelector(".checkout-btn").addEventListener("click", function () {
+  modal.classList.remove("active");
+  document.body.classList.remove("fix");
+});
