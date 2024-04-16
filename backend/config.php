d6e583f0456e9dde19b78df61d8c76c56fade3dc
IbrahimMoatazMohamed <ibrahim.mohamed@stu.ibu.edu.ba>
2024-04-16 22:10:36 +0200

    work

---
+++ backend/config.php
@@ -0,0 +1,13 @@
+<?php
+
+// Set the reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL ^ (E_NOTICE | E_DEPRECATED));
+
+// Database access credentials
+define('DB_NAME', 'sql11699000');
+define('DB_PORT', 3306);
+define('DB_USER', 'sql11699000');
+define('DB_PASSWORD', 'lP4NyUqiKJ');
+define('DB_HOST', 'sql11.freesqldatabase.com'); // localhost
---
+++ utils/constants.js
@@ -0,0 +1,4 @@
+var Constants = {
+  API_BASE_URL:
+    "http://localhost/IT-207-Introduction-to-Web-Programming/backend/",
+};
