1782b4b86392274c717fe75ae0da42084e9d0851
IbrahimMoatazMohamed <ibrahim.mohamed@stu.ibu.edu.ba>
2024-06-09 19:43:07 +0200

    Custom Theme

--- src/component/global/item/itemCardComponent.js
+++ src/component/global/item/itemCardComponent.js
@@ -1,30 +1,60 @@
 import * as React from "react";
-import AspectRatio from "@mui/joy/AspectRatio";
-import Card from "@mui/joy/Card";
-import CardContent from "@mui/joy/CardContent";
-import Typography from "@mui/joy/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 import styles from "./item.module.css";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#007bff",
+    },
+    secondary: {
+      main: "#6c757d",
+    },
+  },
+  typography: {
+    fontFamily: "Roboto, sans-serif",
+  },
+});
 
 const ItemCard = ({ price, imgSrc, name, category }) => {
   return (
-    <Card sx={{ width: 170 }} size="sm" style={{ margin: "0 10px" }}>
-      <AspectRatio minHeight="130px" maxHeight="170px">
-        <img src={imgSrc} alt={category + " Image"} />
-      </AspectRatio>
+    <ThemeProvider theme={theme}>
+      <Card sx={{ width: 170 }} style={{ margin: "0 10px" }}>
+        <Box
+          sx={{
+            minHeight: "130px",
+            maxHeight: "170px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            overflow: "hidden",
+          }}
+        >
+          <img
+            src={imgSrc}
+            alt={category + " Image"}
+            style={{ maxWidth: "100%" }}
+          />
+        </Box>
 
-      <div>
-        <Typography level="title-lg" align="center">
-          {name}
-        </Typography>
-        <Typography level="body-sm" align="center">
-          Price: {price} KM/day
-        </Typography>
-      </div>
+        <CardContent>
+          <Typography variant="h6" align="center">
+            {name}
+          </Typography>
+          <Typography variant="body2" align="center">
+            Price: {price} KM/day
+          </Typography>
+        </CardContent>
 
-      <CardContent orientation="horizontal">
-        <button className={styles.itemBut}></button>
-      </CardContent>
-    </Card>
+        <CardContent>
+          <button className={styles.itemBut}></button>
+        </CardContent>
+      </Card>
+    </ThemeProvider>
   );
 };
 
